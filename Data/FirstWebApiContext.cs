e7bcec7a3ef1f06ff5d5db3a049508506eb78fce
RawadAltagiuri <rawadaltagury@gmail.com>
2023-09-14 18:43:37 +0200

    commit

---
+++ Data/FirstWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FirstWebApi;
+
+namespace FirstWebApi.Data
+{
+    public class FirstWebApiContext : DbContext
+    {
+        public FirstWebApiContext (DbContextOptions<FirstWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstWebApi.CustomersClass> CustomersClass { get; set; } = default!;
+    }
+}
