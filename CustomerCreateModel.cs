27952149fd8eb2748d22da46fe7f454e6f4b2a69
Rawad <120518954+RawadAltagiuri@users.noreply.github.com>
2023-09-14 19:31:01 +0300

    TEST

---
+++ CustomerCreateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstWebApi
+{
+    public class CustomerCreateModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ CustomersClass.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebApi
+{
+    public class CustomersClass
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+    }
+}
